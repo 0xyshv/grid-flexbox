1bbd7b7089b7f34dff9474989372fd81b8c19589
yshv <yashasvichaudhary14@gmail.com>
2023-05-21 18:41:38 +0530

    add about

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./about.module.css"
+
+const about = () => {
+  return (
+    <div className={styles.green}>about</div>
+  )
+}
+
+export default about
